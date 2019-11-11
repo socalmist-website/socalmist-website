b794d1ebbc0c06c9534078494d150be6d4f84b5b
Ali Rehman <rehmanaa@uci.edu>
2019-11-10 22:32:36 -0800

    Hide firebase API key

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,15 +2,13 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyB1T2rjyfv07jgTPsOmFRiarjAX3md3lQM",
-    authDomain: "socalmist-52b6d.firebaseapp.com",
-    databaseURL: "https://socalmist-52b6d.firebaseio.com",
-    projectId: "socalmist-52b6d",
-    storageBucket: "socalmist-52b6d.appspot.com",
-    messagingSenderId: "488845003042",
-    appId: "1:488845003042:web:0863247baf4ba28fbfbd7b",
-    measurementId: "G-VB9XHFWG8L"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
 class Firebase {
   constructor() {
     app.initializeApp(config);
