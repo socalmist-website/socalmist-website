4868faedd7d62e8b626d5e56ec460bdecdebffb6
Ali Rehman <ali67219@gmail.com>
2019-11-29 18:01:35 -0800

    Improve competition info structure

--- src/components/Competition/CompetitionPage.js
+++ src/components/Competition/CompetitionPage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Header } from '../Header/Navbar';
 import queryString from 'query-string';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
 import {Container, Row, Col} from 'react-bootstrap';
 import './CompetitionPage.css';
 import {competitions} from './CompetitionTextConstants';
@@ -10,8 +8,8 @@ import {competitions} from './CompetitionTextConstants';
 class CompetitionPage extends React.Component {
 
   render() {
-    let params = queryString.parse(this.props.location.search);
-    let competitionInfo = competitions[params.competitionName];
+    const params = queryString.parse(this.props.location.search);
+    const competitionInfo = competitions[params.competitionName];
     
     if (competitionInfo == null) {
       return (
@@ -22,17 +20,6 @@ class CompetitionPage extends React.Component {
       )
     }
 
-    let resources = null;
-    if (competitionInfo.hasResources) {
-      resources = (
-        <Tab eventKey="resources" title="Resources">
-          <div className="tabContent">
-            {competitionInfo.resources}
-          </div>
-        </Tab>
-      );
-    }
-
     return (
       <div>
         <Header />
@@ -40,19 +27,7 @@ class CompetitionPage extends React.Component {
           <Row className="justify-content-sm-center">
             <Col sm={6} className="competitionTitle"><h1>{competitionInfo.title}</h1></Col>
             <Col sm={8} id="tabsHolder">
-            <Tabs className="tabText" defaultActiveKey="overview" id="tab">
-              <Tab eventKey="overview" title="Overview">
-                <div className="tabContent">
-                  <p>{competitionInfo.overview}</p>
-                </div>
-              </Tab>
-              <Tab eventKey="rules" title="Rules and Regulations">
-                <div className="tabContent">
-                  {competitionInfo.rules}
-                </div>
-              </Tab>
-              {resources}
-            </Tabs>
+              {competitionInfo.tabs}
             </Col>
           </Row>
         </Container>
--- src/components/Competition/CompetitionTextConstants.js
+++ src/components/Competition/CompetitionTextConstants.js
@@ -1,44 +1,60 @@
 import React from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
 const competitions = {
   "knowledgeTests" : {
     "title" : "Knowledge Tests",
-    "overview" : "Read one of four books and take a test on it. The test comprises of a set of questions and an essay portion to judge reading and comprehension.",
-    "rules" : 
-      <div>
-        <h3>
-          Procedures:
-        </h3>
-        <ol>
-          <li>
-            No more than four (4) students from each team may register for each test, and each student may pick only one
-            test to compete in. For example, 4 students may register for the first test, 4 other students may register for the
-            second test, etc.
-          </li>
-          <li>
-            Competitors must achieve a score of 65% in order to rank/win a medal.
-          </li>
-          <li>
-            Each student will be given a written test containing possible combinations of multiple choice, short answer, and
-            essay question(s) at the time of the competition. Questions will have an analytical/thinking focus.
-          </li>
-          <li>
-            Tests are open-book and must be taken individually.
-          </li>
-          <li>
-            Students may not bring additional notes into the exam room.
-          </li>
-          <li>
-            All questions for each topic will be based on the material covered in the assigned book.
-          </li>
-          <li>
-            There is no ballot for this competition. Scores are based solely on test performance.
-          </li>
-        </ol>
-      <p><b>Note:</b> The books can be found at many online bookstores.</p>
-    </div>,
-    "hasResources" : true,
-    "resources" : <h3>Knowledge Test resources</h3>
+    "tabs" : (
+      <Tabs className="tabText" defaultActiveKey="overview" id="tab">
+        <Tab eventKey="overview" title="Overview">
+          <div className="tabContent">
+            <p>Read one of four books and take a test on it. The test comprises of a set of questions and an essay portion to judge reading and comprehension.</p>
+          </div>
+        </Tab>
+        <Tab eventKey="rules" title="Rules and Regulations">
+          <div className="tabContent">
+            <div>
+              <h3>
+                Procedures:
+              </h3>
+              <ol>
+                <li>
+                  No more than four (4) students from each team may register for each test, and each student may pick only one
+                  test to compete in. For example, 4 students may register for the first test, 4 other students may register for the
+                  second test, etc.
+                </li>
+                <li>
+                  Competitors must achieve a score of 65% in order to rank/win a medal.
+                </li>
+                <li>
+                  Each student will be given a written test containing possible combinations of multiple choice, short answer, and
+                  essay question(s) at the time of the competition. Questions will have an analytical/thinking focus.
+                </li>
+                <li>
+                  Tests are open-book and must be taken individually.
+                </li>
+                <li>
+                  Students may not bring additional notes into the exam room.
+                </li>
+                <li>
+                  All questions for each topic will be based on the material covered in the assigned book.
+                </li>
+                <li>
+                  There is no ballot for this competition. Scores are based solely on test performance.
+                </li>
+              </ol>
+              <p><b>Note:</b> The books can be found at many online bookstores.</p>
+            </div>
+          </div>
+        </Tab>
+        <Tab eventKey="books" title="2020 Books">
+          <div className="tabContent">
+            2020 books to be added
+          </div>
+        </Tab>
+      </Tabs>
+    )
   }
 }
 
