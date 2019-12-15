e308d004718bdc9faf7aba8947dd9d9583b9fdfb
Ali Rehman <rehmanaa@uci.edu>
2019-12-14 21:36:23 -0800

    Register and submissions

--- src/components/Register/RegisterPage.js
+++ src/components/Register/RegisterPage.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { Header } from '../Header/Navbar';
+import React from "react";
+import { Header } from "../Header/Navbar";
+import { PageTitle } from "../Header/PageTitle";
+import "./RegisterPage.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 class RegisterPage extends React.Component {
-
+  styles = {
+    fontWeight: "bold"
+  };
   render() {
+    const image = require("../../public/ambassadors.jpg");
+
     return (
       <div>
         <Header />
-        <h1>Hello World Register</h1>
+        <PageTitle pageTitle="Registration Steps" />
+        <Container id="registrationContainer">
+          <Row className="justify-content-sm-center">
+            <Col sm={7}>
+              <h2>Step 1</h2>
+              <Image fluid={true} src={image} />
+            </Col>
+          </Row>
+          <Row className="justify-content-sm-center">
+            <Col sm={7}>
+              <h2>Step 2</h2>
+              <Image fluid={true} src={image} />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
