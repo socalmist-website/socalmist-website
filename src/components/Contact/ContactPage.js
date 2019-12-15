02c74ca4c7df7b1b4d45709427b751bb3bcae3f8
Ali Rehman <rehmanaa@uci.edu>
2019-12-15 11:02:36 -0800

    Add contact form and email list button

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -12,9 +12,7 @@ class ContactPage extends React.Component {
         <PageTitle pageTitle="Contact" />
         <Row className="justify-content-sm-center">
             <Col sm="7" style={{"text-align": "center", "font-weight":"bold", "margin-top":"2em"}}>
-              <h3>
-                If you would like to contact us, please email us at socal@getmistified.com.
-              </h3>
+            <iframe title="contactForm" style={{"width":"100%"}} src="https://docs.google.com/forms/d/e/1FAIpQLSdJPZPOYnoUhayld2egI7wbOZ1Y1GPCOM2eGTCaYpd3mAwDcA/viewform?embedded=true" width="640" height="790" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </Col>
         </Row>
       </div>
--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -6,18 +6,28 @@ import {Container, Row, Col} from 'react-bootstrap';
 import windowSize from 'react-window-size';
 
 class Banner extends Component {
+
+  componentDidMount() {
+    if (this.props.windowWidth < 576) {
+      document.getElementById("backgroundPicture").setAttribute("style", "height:740px");
+    }
+  }
+
   render() {
     const appstoreImage = require('../../public/appstore.png');
     const playstoreImage = require('../../public/playstore.png');
     let icons = (
       <Container className="iconContainer">
         <Row>
-          <Col sm={6} className="leftIconBlock">
+          <Col sm={4} className="leftIconBlock">
             <SocialIcon target="_blank" className="socialMediaIcon" url="https://www.facebook.com/SoCalMIST/" />
             <SocialIcon target="_blank" className="socialMediaIcon" url="https://www.instagram.com/socalmist/" />
             <SocialIcon target="_blank" className="socialMediaIcon" url="https://twitter.com/socalmist?lang=en" />
           </Col>
-          <Col sm={6} className="rightIconBlock">
+          <Col sm={4} style={{"text-align":"center"}}>
+            <Button href="https://docs.google.com/forms/d/1M7iyi4tL9WI4nfo7mC4ZKYmKxrDsjpGnwyglmcKxf3I/edit?fbclid=IwAR1UCVIzu67NRl3q1DWIgLXCM3lEnK1K8f2y30mxPHydgW1lq3bXUkdPwNc" target="_blank" id="bannerButton" variant="light">Join Our Mailing List!</Button>
+          </Col>
+          <Col sm={4} className="rightIconBlock">
             <a target="_blank" rel="noopener noreferrer" href="https://apps.apple.com/us/app/get-mistified/id1341732029"><img className="appImage" alt="Apple App Store" src={appstoreImage} /></a>
             <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.squarespace.getmistified"><img className="appImage" alt="Google Play Store" src={playstoreImage} /></a>
           </Col>
@@ -39,13 +49,16 @@ class Banner extends Component {
               <a href="https://apps.apple.com/us/app/get-mistified/id1341732029"><img className="appImage" alt="Apple App Store" src={appstoreImage} /></a>
               <a href="https://play.google.com/store/apps/details?id=com.squarespace.getmistified"><img className="appImage" alt="Google Play Store" src={playstoreImage} /></a>
             </Col>
+            <Col xs={12} className="centerAlignIcons">
+              <Button href="https://docs.google.com/forms/d/1M7iyi4tL9WI4nfo7mC4ZKYmKxrDsjpGnwyglmcKxf3I/edit?fbclid=IwAR1UCVIzu67NRl3q1DWIgLXCM3lEnK1K8f2y30mxPHydgW1lq3bXUkdPwNc" target="_blank" id="bannerButton" variant="light">Join Our Mailing List!</Button>
+            </Col>
           </Row>
         </Container>
       )
     }
 
     return (
-      <div className="backgroundPicture">
+      <div id="backgroundPicture">
         <div className="bannerInfoHolder">
           <h1 className="bannerText">SoCal MIST 2020</h1>
         </div>
