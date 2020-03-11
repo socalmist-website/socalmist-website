48087ec1026f856c5ed40f19289a35d668b974e3
Ali Rehman <ali67219@gmail.com>
2020-03-11 09:16:49 -0700

    Put early submissions button on the front page

--- src/components/Board/BoardPage.js
+++ src/components/Board/BoardPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from '../Header/Navbar';
 import { PageTitle } from '../Header/PageTitle';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import './BoardPage.css';
 
 class BoardPage extends React.Component {
@@ -48,12 +48,12 @@ class BoardPage extends React.Component {
         <Container style={{ "margin-top": "3em" }}>
           <Row className="justify-content-sm-center">
             <Col lg={4} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img className = "top2" src={maria_director} height="250px"></img>
+              <img alt="img" className = "top2" src={maria_director} height="250px"></img>
               <h4 className="boardMemName">Maria Najam</h4>
               <h5 className="boardPos">Regional Director</h5>
             </Col>
             <Col lg={4} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img className ="top2" src={salma_director} height="250px"></img>
+              <img alt="img" className ="top2" src={salma_director} height="250px"></img>
               <h4 className="boardMemName">Salma Nasroodeen</h4>
               <h5 className="boardPos">Associate Director</h5>
             </Col>
@@ -62,17 +62,17 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "3em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Anam Aslam</h4>
               <h5 className="boardPos">Programs Chair</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "3em" }}>
-              <img src={mouwaz_finance} height="200px"></img>
+              <img alt="img" src={mouwaz_finance} height="200px"></img>
               <h4 className="boardMemName">Mouaz Kanawati</h4>
               <h5 className="boardPos">Finance Chair</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "3em" }}>
-              <img src={arwa_regis} height="200px"></img>
+              <img alt="img" src={arwa_regis} height="200px"></img>
               <h4 className="boardMemName">Arwa Khan</h4>
               <h5 className="boardPos">Registration Chair</h5>
             </Col>
@@ -80,17 +80,17 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Ennis Machta</h4>
               <h5 className="boardPos">Marketing Chair</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Zara Mann</h4>
               <h5 className="boardPos">Outreach Chair</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={amr_volun} height="200px"></img>
+              <img alt="img" src={amr_volun} height="200px"></img>
               <h4 className="boardMemName">Amr Moussa</h4>
               <h5 className="boardPos">Volunteers Chair</h5>
             </Col>
@@ -99,19 +99,19 @@ class BoardPage extends React.Component {
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
 
-          <img src={raahima_logistcs} className ="boardPic" height="200px"></img>
+          <img alt="img" src={raahima_logistcs} className ="boardPic" height="200px"></img>
                 <h4 className="boardMemName">Raahima Shoaib</h4>
               <h5 className="boardPos">Logistics Chair</h5>
             
               
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={sumaya_comp} height="200px"></img>
+              <img alt="img" src={sumaya_comp} height="200px"></img>
               <h4 className="boardMemName">Sumaya Alkatib</h4>
               <h5 className="boardPos">Category Head</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={samah_comp} height="200px"></img>
+              <img alt="img" src={samah_comp} height="200px"></img>
               <h4 className="boardMemName">Samah Bajwa</h4>
               <h5 className="boardPos">Category Head</h5>
             </Col>
@@ -119,17 +119,17 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={juhaina_comp} height="200px"></img>
+              <img alt="img" src={juhaina_comp} height="200px"></img>
               <h4 className="boardMemName">Juhaina Habeebulla</h4>
               <h5 className="boardPos">Category Head</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={zainab_comp} height="200px"></img>
+              <img alt="img" src={zainab_comp} height="200px"></img>
               <h4 className="boardMemName">Zainab Khan</h4>
               <h5 className="boardPos">Category Head</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={saalim_comp} height="200px"></img>
+              <img alt="img" src={saalim_comp} height="200px"></img>
               <h4 className="boardMemName">Saalim Majoo</h4>
               <h5 className="boardPos">Category Head</h5>
             </Col>
@@ -137,17 +137,17 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={faaizah_comp} height="200px"></img>
+              <img alt="img" src={faaizah_comp} height="200px"></img>
               <h4 className="boardMemName">Faaizah Saiduddin</h4>
               <h5 className="boardPos">Category Head</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={meraal_comp} height="200px"></img>
+              <img alt="img" src={meraal_comp} height="200px"></img>
               <h4 className="boardMemName">Meraal Abutaha</h4>
               <h5 className="boardPos">Category Head</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Nimah Khan</h4>
               <h5 className="boardPos">Spirits Competitions Coordinator</h5>
             </Col>
@@ -155,18 +155,18 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
           <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-            <img src={sadaf_comp} height="200px"></img>
+            <img alt="img" src={sadaf_comp} height="200px"></img>
               <h4 className="boardMemName">Sadaf Qadir</h4>
               <h5 className="boardPos">Competitions Scorekeeper</h5>
              </Col>
             
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Insha Khan</h4>
               <h5 className="boardPos">Workshops Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Mariam Masud</h4>
               <h5 className="boardPos">Spirits Competitions Coordinator</h5>
             </Col>
@@ -174,17 +174,17 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Yusuf Ezzeldine</h4>
               <h5 className="boardPos">Outreach Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Aatena Hasan</h4>
               <h5 className="boardPos">Outreach Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={asra_logistics} height="200px"></img>
+              <img alt="img" src={asra_logistics} height="200px"></img>
               <h4 className="boardMemName">Asra Fahim</h4>
               <h5 className="boardPos">Communications Coordinator</h5>
             </Col>
@@ -192,17 +192,17 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={asad_logistics} height="200px"></img>
+              <img alt="img" src={asad_logistics} height="200px"></img>
               <h4 className="boardMemName">Asad Yazdani</h4>
               <h5 className="boardPos">Logistics Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={baraa_logistics} height="200px"></img>
+              <img alt="img" src={baraa_logistics} height="200px"></img>
               <h4 className="boardMemName">Baraa Zekeria</h4>
               <h5 className="boardPos">Logistics Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={sahila_regis} height="200px"></img>
+              <img alt="img" src={sahila_regis} height="200px"></img>
               <h4 className="boardMemName">Sahila Kachroo</h4>
               <h5 className="boardPos">Registration Coordinator</h5>
             </Col>
@@ -210,17 +210,17 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={sana_regis} height="200px"></img>
+              <img alt="img" src={sana_regis} height="200px"></img>
               <h4 className="boardMemName">Sana Khan</h4>
               <h5 className="boardPos">Registration Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={summer_regis} height="200px"></img>
+              <img alt="img" src={summer_regis} height="200px"></img>
               <h4 className="boardMemName">Summer Kurdi</h4>
               <h5 className="boardPos">Registration Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={husna_finance} height="200px"></img>
+              <img alt="img" src={husna_finance} height="200px"></img>
               <h4 className="boardMemName">Husna Maiwandi</h4>
               <h5 className="boardPos">Sponsorships Coordinator</h5>
             </Col>
@@ -228,17 +228,17 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Iqra Abbaali</h4>
               <h5 className="boardPos">Media Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Shereen Nourollahi</h4>
               <h5 className="boardPos">Social Media Coordinator</h5>
             </Col>
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Ali Rehman</h4>
               <h5 className="boardPos">Webmaster</h5>
             </Col>
@@ -246,13 +246,13 @@ class BoardPage extends React.Component {
 
           <Row className="justify-content-sm-center">           
             <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={image} height="200px"></img>
+              <img alt="img" src={image} height="200px"></img>
               <h4 className="boardMemName">Hamza Ayub</h4>
               <h5 className="boardPos">Ceremony Coordinator</h5>
             </Col>
            
               <Col lg={3} style={{ 'text-align': 'center', "margin-top": "2em" }}>
-              <img src={sara_comp} height="200px"></img>
+              <img alt="img" src={sara_comp} height="200px"></img>
               <h4 className="boardMemName">Sara Anis</h4>
               <h5 className="boardPos">Competitions Scorekeeper</h5>
             </Col>
--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "./Banner.css";
-import {SocialIcon} from 'react-social-icons';
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import { SocialIcon } from 'react-social-icons';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import windowSize from 'react-window-size';
 
 class Banner extends Component {
@@ -17,74 +17,67 @@ class Banner extends Component {
     const playstoreImage = require('../../public/playstore.png');
     let buttons = (
       <Container fluid={true}>
-          <Row className="justify-content-sm-center buttonRow">
-            <Col lg={3} style={{'text-align':'center'}}>
+        <Row className="justify-content-sm-center buttonRow">
+          <Col lg={3} style={{ 'text-align': 'center' }}>
             <a className="buttonLink" target="_blank" rel="noopener noreferrer" href="https://my.getmistified.com">
               {/* <div className = 'borderBox'> </div> */}
-                  <div className='buttonDiv'>
-                  
-                    <span className='buttonText'><b>Register</b></span>
-                    
-                    </div>
-                  
-              </a>
-              
-                
-            </Col>
-            <Col lg={3} style={{'text-align':'center'}}>
-              <a className="buttonLink" target="_blank" rel="noopener noreferrer" href="https://www.getmistified.com/news/2020/1/13/2020-competitions-rulebook">
-              {/* <div className = 'borderBox'> </div> */}
-               
-                <div className='buttonDiv'>
-                <span className='buttonText'><b>2020 Rulebook</b></span>
+              <div className='buttonDiv'>
+                <span className='buttonText'><b>Register</b></span>
+              </div>
+            </a>
 
-                
-                </div>
-              </a>
-              
-            </Col>
-            <Col lg={3} className="centerAlign">
-              <a className="buttonLink" href="https://socalmist.com/8th-grade" target="_blank" rel="noopener noreferrer">
+
+          </Col>
+          <Col lg={3} style={{ 'text-align': 'center' }}>
+            <a className="buttonLink" target="_blank" rel="noopener noreferrer" href="https://www.getmistified.com/news/2020/1/13/2020-competitions-rulebook">
               {/* <div className = 'borderBox'> </div> */}
-               
-                <div className='buttonDiv'>
-                  <span className='buttonText'><b>8th Grade App</b></span>
-                </div>
-              </a>
-            </Col>
-          </Row>
-          <Row className="justify-content-sm-center buttonRow">
-            <Col lg={3} className="centerAlign">
-              <a className="buttonLink" href="https://socalmist.com/judge" target="_blank" rel="noopener noreferrer">
+              <div className='buttonDiv'>
+                <span className='buttonText'><b>2020 Rulebook</b></span>
+              </div>
+            </a>
+
+          </Col>
+          <Col lg={3} className="centerAlign">
+            <a className="buttonLink" href="https://socalmist.com/earlySubmissions" target="_blank" rel="noopener noreferrer">
               {/* <div className = 'borderBox'> </div> */}
-               
-                <div className='buttonDiv'>
-                  <span className='buttonText'><b>Judge</b></span>
-                </div>
-              </a>
-              
-            </Col>
-            <Col lg={3} className="centerAlign">
-              <a className="buttonLink" target="_blank" rel="noopener noreferrer" href="https://socalmist.com/volunteer">
+              <div className='buttonDiv'>
+                <span className='buttonText'><b>Early Submissions</b></span>
+              </div>
+            </a>
+          </Col>
+        </Row>
+        <Row className="justify-content-sm-center buttonRow">
+          <Col lg={3} className="centerAlign">
+            <a className="buttonLink" href="https://socalmist.com/judge" target="_blank" rel="noopener noreferrer">
               {/* <div className = 'borderBox'> </div> */}
-               
-                <div className='buttonDiv'>
+
+              <div className='buttonDiv'>
+                <span className='buttonText'><b>Judge</b></span>
+              </div>
+            </a>
+
+          </Col>
+          <Col lg={3} className="centerAlign">
+            <a className="buttonLink" target="_blank" rel="noopener noreferrer" href="https://socalmist.com/volunteer">
+              {/* <div className = 'borderBox'> </div> */}
+
+              <div className='buttonDiv'>
                 <span className='buttonText'><b>Volunteer</b></span>
-                </div>
-              </a>
-              
-            </Col>
-            <Col lg={3} className="centerAlign">
-              <a className="buttonLink" target="_blank" rel="noopener noreferrer" href="http://localhost:3000/financial-aid">
+              </div>
+            </a>
+
+          </Col>
+          <Col lg={3} className="centerAlign">
+            <a className="buttonLink" target="_blank" rel="noopener noreferrer" href="http://localhost:3000/financial-aid">
               {/* <div className = 'borderBox'> </div> */}
-               
-                <div className='buttonDiv'>
+
+              <div className='buttonDiv'>
                 <span className='buttonText'><b>Financial Aid App</b></span>
-                </div>
-              </a>
-            </Col>
-          </Row>
-        </Container>
+              </div>
+            </a>
+          </Col>
+        </Row>
+      </Container>
     );
     let icons = (
       <Container className="iconContainer">
@@ -94,7 +87,7 @@ class Banner extends Component {
             <SocialIcon target="_blank" className="socialMediaIcon" url="https://www.instagram.com/socalmist/" />
             <SocialIcon target="_blank" className="socialMediaIcon" url="https://twitter.com/socalmist?lang=en" />
           </Col>
-          <Col sm={4} style={{"text-align":"center"}}>
+          <Col sm={4} style={{ "text-align": "center" }}>
           </Col>
           <Col sm={4} className="rightIconBlock">
             <a target="_blank" rel="noopener noreferrer" href="https://apps.apple.com/us/app/get-mistified/id1341732029"><img className="appImage" alt="Apple App Store" src={appstoreImage} /></a>
@@ -105,10 +98,10 @@ class Banner extends Component {
     );
     let mainTitle = (
       <div className="bannerInfoHolder">
-          <h1 className="bannerText">SoCal MIST 2020</h1>
+        <h1 className="bannerText">SoCal MIST 2020</h1>
       </div>
     );
-      
+
 
     if (this.props.windowWidth < 992) {
       icons = (
@@ -125,7 +118,7 @@ class Banner extends Component {
               <a href="https://play.google.com/store/apps/details?id=com.squarespace.getmistified"><img className="appImage" alt="Google Play Store" src={playstoreImage} /></a>
             </Col>
             <Col xs={12} className="centerAlignIcons">
-              <Button href="https://docs.google.com/forms/d/1M7iyi4tL9WI4nfo7mC4ZKYmKxrDsjpGnwyglmcKxf3I/edit?fbclid=IwAR1UCVIzu67NRl3q1DWIgLXCM3lEnK1K8f2y30mxPHydgW1lq3bXUkdPwNc" target="_blank" style={{"font-size": "1.5em"}} variant="light">Join Our Mailing List!</Button>
+              <Button href="https://docs.google.com/forms/d/1M7iyi4tL9WI4nfo7mC4ZKYmKxrDsjpGnwyglmcKxf3I/edit?fbclid=IwAR1UCVIzu67NRl3q1DWIgLXCM3lEnK1K8f2y30mxPHydgW1lq3bXUkdPwNc" target="_blank" style={{ "font-size": "1.5em" }} variant="light">Join Our Mailing List!</Button>
             </Col>
           </Row>
         </Container>
@@ -133,12 +126,12 @@ class Banner extends Component {
       buttons = (
         <div className="bannerSubInfoHolder">
           <h2 className="bannerSubHeader">Early Bird Registration is Now Open!</h2>
-            <Button style={{"font-size": "1.5em"}} variant="light" href="https://my.getmistified.com" target="_blank">Register</Button>
+          <Button style={{ "font-size": "1.5em" }} variant="light" href="https://my.getmistified.com" target="_blank">Register</Button>
         </div>
       );
       mainTitle = (
         <div className="bannerInfoHolder">
-            <h1 className="bannerTextSmall">SoCal MIST 2020</h1>
+          <h1 className="bannerTextSmall">SoCal MIST 2020</h1>
         </div>
       );
     }
@@ -152,5 +145,5 @@ class Banner extends Component {
     );
   }
 }
- 
+
 export default windowSize(Banner);
