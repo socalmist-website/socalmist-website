4310a19fa429635283ed7481576dd752df302182
M. Ibrahim Israr <israr.m.ibrahim@gmail.com>
2023-01-22 21:35:49 -0800

    Rulebook Supplement Final Update

--- src/components/Rulebook/RulebookPage.js
+++ src/components/Rulebook/RulebookPage.js
@@ -28,7 +28,7 @@ class RulebookPage extends React.Component {
           <Row className="justify-content-sm-center">
             <Col sm={8} style={{ "text-align": "center", "margin-top": "1.2em" }}>
               <h5>
-                <a href = "https://drive.google.com/file/d/1pPQJ9ANZef3_BTNZpIuY59camEhFWkN3/view?usp=sharing">Click Here to Download!</a>
+                <a href = "https://drive.google.com/file/d/1yg0BUNUr5onTVdXGY2cmMm3k1JL9Z1M1/view?usp=sharing">Click Here to Download!</a>
               </h5>
             </Col>
           </Row>
