4ad84c5fbc21c00df5a1fcf67ad599bbaf183b7a
M. Ibrahim Israr <israr.m.ibrahim@gmail.com>
2023-02-15 09:46:51 -0800

    Restore to initial spirit page

--- src/components/Spirit/SpiritPage.js
+++ src/components/Spirit/SpiritPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from "../Header/Navbar";
 import { PageTitle } from "../Header/PageTitle";
 import { PageSubTitle } from "../Header/PageSubTitle";
@@ -34,19 +33,18 @@ class SpiritPage extends React.Component {
           </Row> */}
           <Row className="justify-content-sm-center">
             <Col className="sponsorFormHolder" style={{ paddingLeft: "4.5em", paddingRight: "2em" }}>
-              <div className="embed-responsive embed-responsive-16by9">
-                <iframe
-                  className="embed-responsive-item"
-                  title="sponsorForm"
-                  style={{ width: "100%", height:"620px", display: "block", "margin-left": "auto", "margin-right": "auto" }}
-                  src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFMIF1DPYYqKaMWUrxzcDiy-wocdC8rDRvLmSnujpcsTTaPm-h0k4AtHFCj3G6jtprQmiBQLze1tde/pubchart?oid=6343663&format=interactive"
-                  frameborder="0"
-                  marginheight="0"
-                  marginwidth="0"
-                >
-                  Loading…
-                </iframe>
-              </div>
+              <iframe
+                title="sponsorForm"
+                style={{ width: "100%", display: "block", "margin-left": "auto", "margin-right": "auto" }}
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFMIF1DPYYqKaMWUrxzcDiy-wocdC8rDRvLmSnujpcsTTaPm-h0k4AtHFCj3G6jtprQmiBQLze1tde/pubchart?oid=6343663&format=interactive"
+                width="640"
+                height="620"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+              >
+                Loading…
+              </iframe>
             </Col>
           </Row>
           <Row className="justify-content-sm-center">
